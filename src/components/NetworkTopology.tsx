7316b7ae2be38a752628c2034922f618a626970b
tanaytr <[200~thetanaytr@gmail.com~>
2025-10-02 23:47:42 +0530

    fix: resolve syntax errors and improve layout in NetworkTopology

--- src/components/NetworkTopology.tsx
+++ src/components/NetworkTopology.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { motion } from 'framer-motion';
 import { Device } from '../types';
 import DevicePod from './DevicePod';
